ce8c6101b04461058f7dfbea5167193511827b0e
elisha <elisha123jc@gmail.com>
2023-10-30 09:13:43 -0400

    refactor main.html

--- src/main/java/com/example/book/addresses.js
+++ src/main/java/com/example/book/addresses.js
@@ -10,28 +10,28 @@ $(document).on("submit", "#createBook", function(event) {
             });
         });
 
-        $(document).on("submit",".buddy_submit", function(event) {
-            var num = $(this).class;
-            var data = {
-                "name" : $("#name" + num).val(),
-                "address" : $("#address" + num).val()
-                "phoneNumber" : $("#phoneNumber" + num).val()
-            };
-            event.preventDefault();
+$(document).on("submit",".buddy_submit", function(event) {
+    var num = $(this).class;
+    var data = {
+        "name" : $("#name" + num).val(),
+        "address" : $("#address" + num).val()
+        "phoneNumber" : $("#phoneNumber" + num).val()
+    };
+    event.preventDefault();
 
-            $.ajax({
-                headers: {
-                    Accept: "application/json; charset=utf-8",
-                    "Content-Type": "application/json; charset=utf-8"
-                },
-                type: "POST",
-                url: "addresses/" + num +"/buddy",
-                data: JSON.stringify(data),
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                success: function(response) {
-                    console.log(response);
-                    $("#buddy" + num).append("<p>" + response.name + "</p><br/><p>" + response.address + "</p><br/><br/><p>" + response.phoneNumber + "</p><br/>");
-                }
-            });
-        });
+    $.ajax({
+        headers: {
+            Accept: "application/json; charset=utf-8",
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        type: "POST",
+        url: "addresses/" + num +"/buddy",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            $("#buddy" + num).append("<p>" + response.name + "</p><br/><p>" + response.address + "</p><br/><br/><p>" + response.phoneNumber + "</p><br/>");
+        }
+    });
+});
