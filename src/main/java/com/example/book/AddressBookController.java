9ddd073bb2ea09d14d7b53250b1010da522c7bd0
elisha <elisha123jc@gmail.com>
2023-10-30 13:40:22 -0400

    refactor main.html

--- src/main/java/com/example/book/AddressBookController.java
+++ src/main/java/com/example/book/AddressBookController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,7 +56,7 @@ public class AddressBookController {
         a1.addBuddy(buddy);
         addressBookRepo.save(a1);
 
-        model.addAttribute("BuddyInfos", buddy);
+        model.addAttribute("AddressBooks", addressBookRepo.findAll());
 
         return "redirect:/addresses";
     }
@@ -62,54 +64,54 @@ public class AddressBookController {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//    @GetMapping("/getadd")
-//    public String getAddresses2(Model model){
-//        model.addAttribute("Addresses", addressBookRepo.findAll().toString());
-//        return "addresses";
-//    }
-//
-//    @GetMapping("/getbud")
-//    public String getBuddies(Model model){
-//        model.addAttribute("Addresses", buddyInfoRepo.findAll());
-//        return "buddies";
-//    }
-//
-//    @PostMapping("/addadd")
-//    public String addAddressBooks(Model model){
-//        AddressBook a = new AddressBook();
-//        addressBookRepo.save(a);
-//        model.addAttribute("AddressId", a.getId());
-//        return "addresses";
-//    }
-//
-//    @PostMapping("/addbud")
-//    public String addBuddy(@RequestParam Long id, @RequestParam String name, @RequestParam String address, @RequestParam String phoneNumber, Model model){
-//        Optional<AddressBook> a = addressBookRepo.findById(id);
-//        if(a.isEmpty()) return null;
-//        AddressBook a1 = a.get();
-//        a1.addBuddy(new BuddyInfo(name, address, phoneNumber));
-//        addressBookRepo.save(a1);
-//        model.addAttribute("Addresses", addressBookRepo.findAll());
-//        return "addresses";
-//    }
-//
-//    @DeleteMapping("/deladd")
-//    public String removeAddress(@RequestParam Long id){
-//        Optional<AddressBook> a1 = addressBookRepo.findById(id);
-//        if(a1.isEmpty())return null;
-//        addressBookRepo.deleteById(id);
-//        return "addresses";
-//    }
-//
-//    @DeleteMapping("/delbud")
-//    public String removeBuddy(@RequestParam Long addressId, @RequestParam Long buddyId){
-//        Optional<AddressBook> a1 = addressBookRepo.findById(addressId);
-//        if(a1.isEmpty()) return null;
-//        AddressBook a = a1.get();
-//        Optional<BuddyInfo> b = buddyInfoRepo.findById(buddyId);
-//        if(b.isEmpty()) return null;
-//        a.removeBuddy(b.get());
-//        addressBookRepo.save(a);
-//        return "addresses";
-//    }
+    @GetMapping("/getadd")
+    public String getAddresses2(Model model){
+        model.addAttribute("Addresses", addressBookRepo.findAll().toString());
+        return "addresses";
+    }
+
+    @GetMapping("/getbud")
+    public String getBuddies(Model model){
+        model.addAttribute("Addresses", buddyInfoRepo.findAll());
+        return "buddies";
+    }
+
+    @PostMapping("/addadd")
+    public String addAddressBooks(Model model){
+        AddressBook a = new AddressBook();
+        addressBookRepo.save(a);
+        model.addAttribute("AddressId", a.getId());
+        return "addresses";
+    }
+
+    @PostMapping("/addbud")
+    public String addBuddy(@RequestParam Long id, @RequestParam String name, @RequestParam String address, @RequestParam String phoneNumber, Model model){
+        Optional<AddressBook> a = addressBookRepo.findById(id);
+        if(a.isEmpty()) return null;
+        AddressBook a1 = a.get();
+        a1.addBuddy(new BuddyInfo(name, address, phoneNumber));
+        addressBookRepo.save(a1);
+        model.addAttribute("Addresses", addressBookRepo.findAll());
+        return "addresses";
+    }
+
+    @DeleteMapping("/deladd")
+    public String removeAddress(@RequestParam Long id){
+        Optional<AddressBook> a1 = addressBookRepo.findById(id);
+        if(a1.isEmpty())return null;
+        addressBookRepo.deleteById(id);
+        return "addresses";
+    }
+
+    @DeleteMapping("/delbud")
+    public String removeBuddy(@RequestParam Long addressId, @RequestParam Long buddyId){
+        Optional<AddressBook> a1 = addressBookRepo.findById(addressId);
+        if(a1.isEmpty()) return null;
+        AddressBook a = a1.get();
+        Optional<BuddyInfo> b = buddyInfoRepo.findById(buddyId);
+        if(b.isEmpty()) return null;
+        a.removeBuddy(b.get());
+        addressBookRepo.save(a);
+        return "addresses";
+    }
 }
---
+++ src/main/resources/templates/addresses.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "POST",
+        url: "https://elisha-addressbook-new.azurewebsites.net/addresses"
+        data: {}
+    }).then(function(data, status, jqxhr) {
+       $(".addressBookId").append(data.id);
+       console.log(jqxhr);
+    });
+});
+
+
+$(document).ready(function() {
+    $.ajax({
+        url: "/addresses"
+    }).then(function(data, status, jqxhr) {
+        ${#book_submit}.submit(function() {
+            ${#add_book}.load();
+        }
+    }
+});
+
+
+$(document).on("submit", "#createBook", function(event) {
+    event.preventDefault();
+
+    $.ajax({
+        headers: {
+            Accept: "application/json; charset=utf-8",
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        type: "POST",
+        url: "/addresses",
+        data: {},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            $(".AddressBook").append("AddressBook #: " + data.id);
+        }
+    });
+});
+
+$(document).on("submit",".buddy_submit", function(event) {
+    var num = $(this).class;
+    var data = {
+        "name" : $("#name" + num).val(),
+        "address" : $("#address" + num).val(),
+        "phoneNumber" : $("#phoneNumber" + num).val()
+    };
+    event.preventDefault();
+
+    $.ajax({
+        headers: {
+            Accept: "application/json; charset=utf-8",
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        type: "POST",
+        url: "addresses/" + num +"/buddy",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            $("#buddy" + num).append("<p>" + response.name + "</p><br/><p>" + response.address + "</p><br/><p>" + response.phoneNumber + "</p><br/>");
+        }
+    });
+});
